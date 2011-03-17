84aebb2c4c863a380a38c0a633b600e58f3d4928
CJ Tolton <ctolton@gmail.com>
2011-03-16 17:06:59 -0700

    fix ws

--- redis_proxy/server.lua
+++ redis_proxy/server.lua
@@ -14,42 +14,41 @@ print("Connected to localhost redis")
 
 -- Take some action based on incoming msg
 function parse_message(msg)
-	
-	if msg.method == "set" then
-		print(string.format("setting: %s", msg.arg1))
-		redis:set(msg.arg1, msg.arg2)
-		return "OK"
-	
-	elseif msg.method == "get" then
-		print(string.format("getting: %s", msg.arg1))
-		return redis:get(msg.arg1)
-	
-	elseif msg.method == 'create_sproc' then
-		print(string.format("Creating sproc: %s", msg.arg1))
-		redis:set(msg.arg1, msg.arg2)
-		return "OK"
 
-	elseif msg.method == "call_sproc" then
-		sp = redis:get(msg.sproc_name)
-		sp_f = loadstring(sp)
-		return sp_f()
+    if msg.method == "set" then
+        print(string.format("setting: %s", msg.arg1))
+        redis:set(msg.arg1, msg.arg2)
+        return "OK"
 
-	else
-		return "Invalid method"
+    elseif msg.method == "get" then
+        print(string.format("getting: %s", msg.arg1))
+        return redis:get(msg.arg1)
 
-	end
+    elseif msg.method == 'create_sproc' then
+        print(string.format("Creating sproc: %s", msg.arg1))
+        redis:set(msg.arg1, msg.arg2)
+        return "OK"
 
+    elseif msg.method == "call_sproc" then
+        sp = redis:get(msg.sproc_name)
+        sp_f = loadstring(sp)
+        return sp_f()
+
+    else
+        return "Invalid method"
+
+    end
 end
 
 
 while true do
-	t = s:recv()
+    t = s:recv()
     print(string.format("Received query: '%s'", t))
-    
+
     j = json.decode(t)
     out = parse_message(j)
     if not out then
-    	out = ""
+        out = ""
     end
     s:send(out)
 end
